d311597aed2e5669e110aaff3f29ab59320c17aa
Amer samamh <85877696+a3amr@users.noreply.github.com>
2026-05-30 21:55:58 +0300

    Rename title and change webrtc key in app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
-st.title("Camera Test")
+st.title("WebRTC Test")
 
 ctx = webrtc_streamer(
-    key="camera",
+    key="test",
     media_stream_constraints={
         "video": True,
         "audio": False,
-    },
+    }
 )
+
+st.write("State:", ctx.state.playing if ctx else "No Context")
